93607e816fef3dd3d1dce54fb01160cc6433edbb
Simon Frampton <simon.frampton@bbc.co.uk>
2020-07-01 16:37:39 +0100

    Update supporter.sh

--- supporter.sh
+++ supporter.sh
@@ -1,8 +1,8 @@
 
 # !/bin/bash 
 
-"$1" == $club
-"$2" == $message
+"$1" == CLUB
+"$2" == MESSAGE
 
 if [ "$1" == "" ]; then
     echo ERROR: "Missing param - must supply two params for this script to work"
@@ -13,4 +13,4 @@ else
     exit 1
 fi
 
-echo "${club}: ${message}"
+echo "${CLUB}: ${MESSAGE}"
