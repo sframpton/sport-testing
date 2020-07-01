8723078078a3d7880215ddf85edfaa14554d9264
Simon Frampton <simon.frampton@bbc.co.uk>
2020-07-01 16:39:06 +0100

    assign vars properly

--- supporter.sh
+++ supporter.sh
@@ -1,8 +1,8 @@
 
 # !/bin/bash 
 
-"$1" == CLUB
-"$2" == MESSAGE
+"$1"=CLUB
+"$2"=MESSAGE
 
 if [ "$1" == "" ]; then
     echo ERROR: "Missing param - must supply two params for this script to work"
